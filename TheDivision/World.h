352e8fb30e226dcbc4e0830e7fcb6d7c51fb87a7
Bisss <nvd.biscuit@gmail.com>
2016-04-13 19:37:00 +0200

    Update World.h
    Update for Incursion patch

--- TheDivision/World.h
+++ TheDivision/World.h
@@ -11,7 +11,7 @@ public:
 	char pad_0x002C[0xEC]; //0x002C
 	char m_szName[8]; //0x0118 
 	char pad_0x0120[0x250]; //0x0120
-	Entity** m_pEntities; //0x0370 
+	Entity** m_pEntities; //0x0370 Update 12/04
 	__int32 m_NumOfEntities; //0x0378 
 	__int32 m_MaxNumOfEntities; //0x037C 
 	char pad_0x0380[0x268]; //0x0380
